92369a5138b9b48a8ae44d224bca8ea5a3fe002b
Agus9217 <justinibarrola92@gmail.com>
2024-06-13 13:40:40 -0300

    export hero like component

---
+++ src/app/components/hero/Hero.tsx
@@ -0,0 +1,51 @@
+import { Box, Button, Container, Heading, Text } from "@chakra-ui/react";
+import Image from "next/image";
+import imgHero from '../../assets/iglesia-hero-image.jpeg'
+export const Hero = () => {
+  return (
+    <Box>
+        <Box
+          as={Image}
+          src={imgHero}
+          w={'100%'}
+          h={{ base: 'auto', '2xl': '1080px' }}
+          objectFit={'cover'}
+          rounded={'2xl'}
+          filter={'brightness(65%)'}
+        />
+        <Box
+          position={'absolute'}
+          top={'50%'}
+          left={'50%'}
+          transform={'translate(-50%, -50%)'}
+          color={'white'}
+          textShadow={'0 0 20px black'}
+          textAlign={'center'}
+          borderWidth={'thin'}
+          borderColor={'cyan'}
+          maxW={'80%'}
+          w={'100%'}
+        >
+          <Heading
+            fontSize={{ base: '2xl', sm: '5xl', md: '6xl', lg: '8xl' }}
+          >
+            Iglesia Cristiana en Barrio Nuevo
+          </Heading>
+          <Text
+            maxW={{ base: '80%', md: '60%' }}
+            mx={'auto'}
+            fontSize={{ base: 'xs', md: 'sm', lg: 'lg' }}
+            py={{ base: '0.5rem', md: '2rem' }}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, blanditiis quam porro, iure tenetur ipsa magnam aut
+          </Text>
+          <Button
+            colorScheme={'yellow'}
+            size={{ base: 'xs', md: 'lg' }}
+          >
+            Ver más
+          </Button>
+        </Box>
+      </Box>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import { Box, Button, Container, Heading, Text } from "@chakra-ui/react";
-import Image from "next/image";
-import imgHero from '../app/assets/iglesia-hero-image.jpeg'
+import { Container } from "@chakra-ui/react";
+import { Hero } from "./components";
 
 export default function HomePage() {
   return (
@@ -12,51 +11,7 @@ export default function HomePage() {
       borderWidth={'thin'}
       borderColor={'cyan'}
     >
-      <Box>
-        <Box
-          as={Image}
-          src={imgHero}
-          w={'100%'}
-          h={{ base: 'auto', '2xl': '1080px' }}
-          objectFit={'cover'}
-          rounded={'2xl'}
-          filter={'brightness(65%)'}
-        />
-        <Box
-          position={'absolute'}
-          top={'50%'}
-          left={'50%'}
-          transform={'translate(-50%, -50%)'}
-          color={'white'}
-          textShadow={'0 0 20px black'}
-          textAlign={'center'}
-          borderWidth={'thin'}
-          borderColor={'cyan'}
-          maxW={'80%'}
-          w={'100%'}
-        >
-          <Heading
-            fontSize={{ base: '2xl', md: '6xl', lg: '8xl' }}
-          >
-            Iglesia Cristiana en Barrio Nuevo
-          </Heading>
-          <Text
-            maxW={{ base: '80%', md: '60%' }}
-            mx={'auto'}
-            fontSize={{ base: 'xs', md: 'sm', lg: 'md' }}
-            py={{ base: '0.5rem', md: '2rem' }}
-          >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, blanditiis quam porro, iure tenetur ipsa magnam aut
-          </Text>
-          <Button
-            colorScheme={'yellow'}
-            size={{ base: 'xs', md: 'lg' }}
-          >
-            Ver más
-          </Button>
-        </Box>
-      </Box>
-
+      <Hero />
     </Container>
   );
 }
