d01fe6e8a8dcb3b8a002fed68ecf80ce31cdec81
Agus9217 <justinibarrola92@gmail.com>
2024-07-16 14:18:05 -0300

    Changing some styles

--- src/app/(pages)/estimulos-espirituales/page.tsx
+++ src/app/(pages)/estimulos-espirituales/page.tsx
@@ -1,199 +1,214 @@
 'use client'
 
-import { Box, Button, Card, CardBody, Flex, Heading, ListItem, Spinner, Stack, Text, UnorderedList } from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Card,
+    CardBody,
+    Flex,
+    Heading,
+    ListItem,
+    Spinner,
+    Stack,
+    Text,
+    UnorderedList
+} from "@chakra-ui/react";
 import Image from "next/image";
 import estimulosImg from '../../assets/estimulos-espirituales.jpeg'
 import useSWR from 'swr'
-import { useRef, useState } from "react";
+import {useRef, useState} from "react";
 import Youtube from "@/app/components/youtube/Youtube";
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
+import { IoPlay } from "react-icons/io5";
 
 interface Media {
-  thumbnail_url: string;
-  permalink: string;
-  caption: string;
-  media_url: string;
-  media_type: string;
-  id: string;
+    thumbnail_url: string;
+    permalink: string;
+    caption: string;
+    media_url: string;
+    media_type: string;
+    id: string;
 }
 
 interface ApiResponse {
-  data: Media[];
+    data: Media[];
 }
+
 const URL = process.env.NEXT_PUBLIC_URL_INSTAGRAM
 
 const fetcher = (url: string) => fetch(url).then(res => res.json());
 
 export default function EstimulosPage() {
-  const [selectedIdx, setSelectedIdx] = useState<number | null>(null);
-  const selectedRef = useRef<HTMLVideoElement | null>(null);
-  const { data, error, isLoading } = useSWR<ApiResponse | undefined>(URL, fetcher);
+    const [selectedIdx, setSelectedIdx] = useState<number | null>(null);
+    const selectedRef = useRef<HTMLVideoElement | null>(null);
+    const {data, error, isLoading} = useSWR<ApiResponse | undefined>(URL, fetcher);
 
-  const handleMediaClick = (index: number, event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
-    const currentSelectedVideo = selectedRef.current;
+    const handleMediaClick = (index: number, event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+        const currentSelectedVideo = selectedRef.current;
 
-    if (currentSelectedVideo) {
-      if (selectedIdx === index) {
-        if (!currentSelectedVideo.paused) {
-          currentSelectedVideo.pause();
-          return;
+        if (currentSelectedVideo) {
+            if (selectedIdx === index) {
+                if (!currentSelectedVideo.paused) {
+                    currentSelectedVideo.pause();
+                    return;
+                }
+            } else {
+                currentSelectedVideo.pause();
+            }
         }
-      } else {
-        currentSelectedVideo.pause();
-      }
-    }
 
-    setSelectedIdx(index);
-    selectedRef.current = event.currentTarget.querySelector('video');
-    console.log(selectedRef.current)
-    if (selectedRef.current) {
-      selectedRef.current.play();
-    }
-  };
+        setSelectedIdx(index);
+        selectedRef.current = event.currentTarget.querySelector('video');
+        console.log(selectedRef.current)
+        if (selectedRef.current) {
+            selectedRef.current.play();
+        }
+    };
 
-  const videos = data?.data.filter(media => media.media_type === 'VIDEO').slice(0, 4)
+    const videos = data?.data.filter(media => media.media_type === 'VIDEO').slice(0, 4)
 
-  const ButtonMotion = motion(Button)
+    const ButtonMotion = motion(Button)
 
-  return (
-    <>
-      <Stack
-        position={'relative'}
-        top={'5rem'}
-        direction={{ base: 'column', md: 'row' }}
-        justifyContent={'center'}
-        alignItems={'center'}
-      >
-        <Flex
-          maxW={{ base: '100%', md: '50%' }}
-          flexGrow={1}
-          direction={'column'}
-          alignItems={'center'}
-        >
-          <Box
-            as={Image}
-            maxW={{ base: '200px', md: '400px' }}
-            h={{ base: '200px', md: '400px' }}
-            src={estimulosImg}
-            alt={'Imagen estimulos espirituales'}
-            priority={true}
-          />
-        </Flex>
-        <Flex
-          maxW={{ base: '100%', md: '50%' }}
-          flexGrow={1}
-          direction={'column'}
-          justifyContent={'center'}
-          p={2}
-          textAlign={{ base: 'center', md: 'start' }}
-        >
-          <Heading
-            fontSize={{ base: '3xl', md: '5xl' }}
-            pb={'2rem'}
-          >
-            Estimulos Espirituales
-          </Heading>
-          <Text
-            maxW={{ base: '90%', md: '70%' }}
-            mx={{ base: 'auto', md: 0 }}
-          >
-            Y consideremos como estimularnos unos
-            a otros al Amor y las Buenas obras
-            Hebreos 10:24
-          </Text>
-          <Flex
-            pt={'2rem'}
-            gap={3}
-            justifyContent={{ base: 'center', md: 'start' }}
-          >
-            <Button
-              colorScheme={'red'}
+    return (
+        <>
+            <Stack
+                position={'relative'}
+                top={'5rem'}
+                direction={{base: 'column', md: 'row'}}
+                justifyContent={'center'}
+                alignItems={'center'}
             >
-              YouTube
-            </Button>
-            <Button
-              colorScheme={'purple'}
-            >
-              Ver más
-            </Button>
-          </Flex>
-        </Flex>
-
-      </Stack>
-      <Stack
-        mt={'9rem'}
-        borderWidth={'thin'}
-        borderColor={'cyan'}
-        w={'100%'}
-      >
-        <Youtube />
-      </Stack>
-      <Flex
-        mt={'9rem'}
-        borderWidth={'thin'}
-        borderColor={'red'}
-        w={'100%'}
-        direction={'column'}
-      >
-        <Heading
-          fontSize={{ base: '2xl', md: '4xl' }}
-          py={{ base: 2, md: 8 }}
-        >
-
-        </Heading>
-        {isLoading ? (
-          <Box
-            mx={'auto'}
-          >
-            <Spinner color={'purple'} />
-          </Box>
-        ) : (
-          data && data.data ? (
-            <UnorderedList
-              display={'flex'}
-              styleType={'none'}
-              alignItems={'center'}
-              justifyContent={'center'}
-              gap={'10px'}
-            >
-              {videos?.map((media, index) => (
-                <ListItem key={media.id} onClick={(e) => handleMediaClick(index, e)}>
-                  <Card
-                    w={200}
-                  >
-                    <CardBody>
-                      <Box
-                        h={250}
-                        display={'flex'}
-                        justifyContent={'center'}
-                      >
-                        <Box
-                          h={'100%'}
-                          as={'video'}
-                          src={media.media_url}
-                          poster={media.thumbnail_url}
-                        />
-                      </Box>
-
-                      <Stack mt='6' spacing='3'>
-                        <ButtonMotion
-                          colorScheme={'purple'}
-                          whileTap={{ scale: 0.9 }}
-                          whileHover={{ scale: 1.1 }}
+                <Flex
+                    maxW={{base: '100%', md: '50%'}}
+                    flexGrow={1}
+                    direction={'column'}
+                    alignItems={'center'}
+                >
+                    <Box
+                        as={Image}
+                        maxW={{base: '200px', md: '400px'}}
+                        h={{base: '200px', md: '400px'}}
+                        src={estimulosImg}
+                        alt={'Imagen estimulos espirituales'}
+                        priority={true}
+                    />
+                </Flex>
+                <Flex
+                    maxW={{base: '100%', md: '50%'}}
+                    flexGrow={1}
+                    direction={'column'}
+                    justifyContent={'center'}
+                    p={2}
+                    textAlign={{base: 'center', md: 'start'}}
+                >
+                    <Heading
+                        fontSize={{base: '3xl', md: '5xl'}}
+                        pb={'2rem'}
+                    >
+                        Estimulos Espirituales
+                    </Heading>
+                    <Text
+                        maxW={{base: '90%', md: '70%'}}
+                        mx={{base: 'auto', md: 0}}
+                    >
+                        Y consideremos como estimularnos unos
+                        a otros al Amor y las Buenas obras
+                        Hebreos 10:24
+                    </Text>
+                    <Flex
+                        pt={'2rem'}
+                        gap={3}
+                        justifyContent={{base: 'center', md: 'start'}}
+                    >
+                        <Button
+                            colorScheme={'red'}
                         >
-                          Reproducir
-                        </ButtonMotion>
-                      </Stack>
-                    </CardBody>
-                  </Card>
-                </ListItem>
-              ))}
-            </UnorderedList>
-          ) : (
-            <div>Error: {error}.</div>
-          )
-        )}
-      </Flex >
-    </>
-  )
+                            YouTube
+                        </Button>
+                        <Button
+                            colorScheme={'purple'}
+                        >
+                            Ver más
+                        </Button>
+                    </Flex>
+                </Flex>
+
+            </Stack>
+            <Stack
+                mt={'9rem'}
+                borderWidth={'thin'}
+                borderColor={'cyan'}
+                w={'100%'}
+            >
+                <Youtube/>
+            </Stack>
+            <Flex
+                mt={'9rem'}
+                borderWidth={'thin'}
+                borderColor={'red'}
+                w={'100%'}
+                direction={'column'}
+            >
+                <Heading
+                    fontSize={{base: '2xl', md: '4xl'}}
+                    py={{base: 2, md: 8}}
+                >
+
+                </Heading>
+                {isLoading ? (
+                    <Box
+                        mx={'auto'}
+                    >
+                        <Spinner color={'purple'}/>
+                    </Box>
+                ) : (
+                    data && data.data ? (
+                        <UnorderedList
+                            display={'flex'}
+                            styleType={'none'}
+                            alignItems={'center'}
+                            justifyContent={'center'}
+                            gap={'10px'}
+                        >
+                            {videos?.map((media, index) => (
+                                <ListItem key={media.id} onClick={(e) => handleMediaClick(index, e)}>
+                                    <Card
+                                        w={200}
+                                    >
+                                        <CardBody>
+                                            <Box
+                                                h={250}
+                                                display={'flex'}
+                                                justifyContent={'center'}
+                                            >
+                                                <Box
+                                                    h={'100%'}
+                                                    as={'video'}
+                                                    src={media.media_url}
+                                                    poster={media.thumbnail_url}
+                                                />
+                                            </Box>
+
+                                            <Stack mt='6' spacing='3'>
+                                                <ButtonMotion
+                                                    leftIcon={<IoPlay />}
+                                                    colorScheme={'purple'}
+                                                    whileTap={{scale: 0.9}}
+                                                    whileHover={{scale: 1.1}}
+                                                >
+                                                    Reproducir
+                                                </ButtonMotion>
+                                            </Stack>
+                                        </CardBody>
+                                    </Card>
+                                </ListItem>
+                            ))}
+                        </UnorderedList>
+                    ) : (
+                        <div>Error: {error}.</div>
+                    )
+                )}
+            </Flex>
+        </>
+    )
 }
--- src/app/components/youtube/Youtube.tsx
+++ src/app/components/youtube/Youtube.tsx
@@ -1,56 +1,7 @@
 import { Box, Stack } from '@chakra-ui/react';
 import React from 'react'
 import useSWR from 'swr';
-
-export interface Welcome {
-  kind: string;
-  etag: string;
-  regionCode: string;
-  pageInfo: PageInfo;
-  items: Item[];
-}
-
-export interface Item {
-  kind: string;
-  etag: string;
-  id: ID;
-  snippet: Snippet;
-}
-
-export interface ID {
-  kind: string;
-  videoId?: string;
-  channelId?: string;
-}
-
-export interface Snippet {
-  publishedAt: Date;
-  channelId: string;
-  title: string;
-  description: string;
-  thumbnails: Thumbnails;
-  channelTitle: string;
-  liveBroadcastContent: string;
-  publishTime: Date;
-}
-
-export interface Thumbnails {
-  default: Default;
-  medium: Default;
-  high: Default;
-}
-
-export interface Default {
-  url: string;
-  width?: number;
-  height?: number;
-}
-
-export interface PageInfo {
-  totalResults: number;
-  resultsPerPage: number;
-}
-
+import {Welcome} from "@/app/components/youtube/interfaces";
 
 const fetcher = (url: string) => fetch(url).then(res => res.json());
 const API_YOUTUBE = process.env.NEXT_PUBLIC_URL_YOUTUBE_API_KEY
@@ -61,26 +12,26 @@ console.log(URL)
 export default function Youtube() {
   const { data, isLoading } = useSWR<Welcome | undefined>(URL, fetcher)
 
-  const allData = data?.items
-
+  const allData = data?.items.slice(0, 4)
 
   return (
     <Stack
       direction={'row'}
+      justifyContent={'center'}
+      w={'100%'}
     >
       {
         allData?.map((video, index) => (
-          <Box key={`${video.id.videoId}-${index}`}>
             <Box
               as={'iframe'}
-              width="560"
-              height="315"
+              key={`${video.id.videoId}-${index}`}
+              width={'100%'}
+              height="340"
               src={`https://www.youtube.com/embed/${video.id.videoId}`}
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             >
-            </Box>
           </Box>))
       }
     </Stack>
